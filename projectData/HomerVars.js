1e1dfdd281e719b7296bf61994bd14b2b276288b
Burgalassi Francesco <fraburga@gmail.com>
2024-05-21 13:35:06 +0200

    commit

---
+++ projectData/HomerVars.js
@@ -0,0 +1,103 @@
+/**
+* javascript variables for the Homer project:
+* Aftermath
+* Homer - The Story Flow Editor.
+* Copyright (c)2021-2024. Open Lab s.r.l - Florence, Italy
+* Developer: Pupunzi (Matteo Bicocchi)
+*
+**/
+
+class HomerVars {
+
+      static XAX = 0;
+      static X = 0;
+      static ACTOR_SELF_EXTEEM = 0;
+      static CMD = "text";
+      static SCENE_TYPE = "text";
+      static QUEST_TASK = 0;
+      static DIALOGUE_NAME = "text";
+      static CUT_SCENE_NAME = "text";
+      static EXPRESSION = {"IDLE":"IDLE"," ANGRY":" ANGRY"," HAPPY":" HAPPY"};
+
+};
+
+class HomerActors {
+
+      static NARRATOR = "Narrator";
+      static FUTURE_YOU = "Future You";
+      static PAST_YOU = "Past You";
+      static ___ = "???";
+
+}
+
+class HomerMeta {
+      static Mood = {
+          NEUTRAL : 'NEUTRAL',
+          HAPPY : 'HAPPY',
+          VERY_HAPPY : 'VERY_HAPPY',
+          SAD : 'SAD',
+          ANGRY : 'ANGRY',
+          AMAZED : 'AMAZED',
+      };
+      static Expression = {
+          RELAXED : 'RELAXED',
+          GRINNING : 'GRINNING',
+          PENSIVE : 'PENSIVE',
+          FROWNING : 'FROWNING',
+          CRYING : 'CRYING',
+          ENRAGED : 'ENRAGED',
+      };
+      static Balloon_Type = {
+          SPEECH : 'SPEECH',
+          WHISPER : 'WHISPER',
+          THOUGHT : 'THOUGHT',
+          SCREAM : 'SCREAM',
+          SLEEP : 'SLEEP',
+          SING_SONG : 'SING_SONG',
+      };
+      static Location = {
+          HOME : 'HOME',
+          OUTSIDE : 'OUTSIDE',
+          OFFICE : 'OFFICE',
+          PARK : 'PARK',
+          SEA : 'SEA',
+          MOUNTAIN : 'MOUNTAIN',
+      };
+      static Camera_direction = {
+          CLOSE_UP : 'CLOSE_UP',
+          ANGLE_ON : 'ANGLE_ON',
+          OFF_SCREEN : 'OFF_SCREEN',
+          FADE_OUT : 'FADE_OUT',
+          FADE_IN : 'FADE_IN',
+          VOICE_OVER : 'VOICE_OVER',
+      };
+      static Lightining = {
+          KEY : 'KEY',
+          BACK : 'BACK',
+          PRACTICAL : 'PRACTICAL',
+          HARD : 'HARD',
+          SOFT : 'SOFT',
+          CHIAROSCURO : 'CHIAROSCURO',
+      };
+      static FLOW_STATE = {
+          IDEA : 'IDEA',
+          NOTES : 'NOTES',
+          DRAFT : 'DRAFT',
+          EDITOR : 'EDITOR',
+          FINAL : 'FINAL',
+      };
+
+}
+
+class HomerLabels {
+
+      static NEW_LABEL_KEY = "NEW_LABEL_KEY";
+      static GENERAL = "GENERAL";
+
+}
+
+
+class HomerFlowSlugs {
+      static AFTERMATH = "Aftermath";
+ }
+
